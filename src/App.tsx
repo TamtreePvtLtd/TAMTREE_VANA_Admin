d5ed5ed8a854485f2027b5f3de37ea782546237e
Logeswaran <logeswaran2108@gmail.com>
2024-02-21 15:55:19 +0530

    root change

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
               <Route path={paths.ROOT} element={<Layout />}>
               <Route
                   index
-                  path={paths.ROOT}
+                  path={paths.ORDER}
                   element={
                     <PrivateRoute>
                       <Order />
