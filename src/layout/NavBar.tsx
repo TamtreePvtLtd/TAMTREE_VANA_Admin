2909632e4c6b75df9989743104c69d917d93a395
sathyaprabaS <sathyapraba48@gmail.com>
2024-02-24 16:50:07 +0530

    navbar corrction

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -12,61 +12,66 @@ import {
   Box,
   ListItemIcon,
   Tooltip,
+  Button,
+  useMediaQuery,
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import CategoryIcon from "@mui/icons-material/Category";
-import StoreIcon from "@mui/icons-material/Store";
 import { useNavigate } from "react-router";
 import { paths } from "../routes/path";
 import vanaLogo from "../assets/vanalogo.png";
-import { handleLogout} from "../services/LoginApi";
+import { handleLogout } from "../services/LoginApi";
 import Logout from "@mui/icons-material/Logout";
 import { useAuthContext } from "../context/AuthContext";
-const menuItems = [
-  { text: "Orders", icon: <AddShoppingCartIcon />, path: `${paths.ORDER}` },
-  { text: "Products", icon: <StoreIcon />, path: `${paths.PRODUCT}` },
-  { text: "Collections", icon: <CategoryIcon />, path: `${paths.CATEGORY}` },
-];
+import { Link } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
 
+// const menuItems = [
+//   { text: "Orders", icon: <AddShoppingCartIcon />, path: `${paths.ORDER}` },
+//   { text: "Products", icon: <StoreIcon />, path: `${paths.PRODUCT}` },
+//   { text: "Collections", icon: <CategoryIcon />, path: `${paths.CATEGORY}` },
+// ];
+const navMenus = [
+  {
+    label: "Orders",
+    link: paths.ORDER,
+  },
+  {
+    label: "Products",
+    link: paths.PRODUCT,
+  },
+  {
+    label: "Collections",
+    link: paths.CATEGORY,
+  },
+];
 
 const NavBar = () => {
   const navigate = useNavigate();
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const { user,updateUserData} = useAuthContext();
+  const { user, updateUserData } = useAuthContext();
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-
+  const [isNavBarOpen, setNavBarOpen] = useState(false);
 
   const open = Boolean(anchorEl);
+  const theme = useTheme();
 
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
-
-  const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget as HTMLButtonElement);
+  const handleMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  const handleMenuClose = () => {
+  const handleClose = () => {
     setAnchorEl(null);
   };
 
-
-  
-  const handleDrawerOpen = () => {
-    setDrawerOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setDrawerOpen(false);
+  const handleDrawerToggle = () => {
+    setNavBarOpen((prevState) => !prevState);
   };
 
   const handlePageNavigate = (path: string) => {
     navigate(`${path}`);
-    setDrawerOpen(false);
   };
-
 
   const handleLogoutClick = async () => {
     await handleLogout()
@@ -74,7 +79,7 @@ const NavBar = () => {
         if (response.status) {
           updateUserData(null);
 
-          handleMenuClose();
+          // handleMenuClose();
           navigate(paths.LOGIN);
         }
       })
@@ -85,7 +90,6 @@ const NavBar = () => {
       });
   };
 
-
   return (
     <>
       <AppBar
@@ -93,9 +97,6 @@ const NavBar = () => {
         sx={{ height: "70px", display: "flex", justifyContent: "center" }}
       >
         <Toolbar>
-          <IconButton edge="start" color="inherit" onClick={handleDrawerOpen}>
-            <MenuIcon />
-          </IconButton>
           <Avatar
             alt="Company Logo"
             src={vanaLogo}
@@ -106,14 +107,49 @@ const NavBar = () => {
               width: "45px",
             }}
           />
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            VANA
-          </Typography>
-          
+          <Typography variant="h6">VANA</Typography>
+          <Box
+            sx={{ width: "90%", display: "flex", justifyContent: "flex-end" }}
+          >
+            {navMenus.map((menu, index) => (
+              <Box
+                key={menu.label}
+                sx={{
+                  marginRight: index < navMenus.length - 1 ? "20px" : "0",
+                }}
+              >
+                <Link to={menu.link} style={{ textDecoration: "none" }}>
+                  <Button
+                    sx={{
+                      borderRadius: "50px",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      fontSize: "large",
+                      textTransform: "none",
+                      color: "white",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                      px={2}
+                    >
+                      {menu.label}
+                    </Box>
+                  </Button>
+                </Link>
+              </Box>
+            ))}
+          </Box>
+
           {user && (
             <Tooltip title="Account settings">
               <IconButton
-                onClick={handleMenuOpen}
+                sx={{ marginLeft: "40px" }}
+                onClick={handleMenuClick}
                 size="small"
                 aria-controls={open ? "account-menu" : undefined}
                 aria-haspopup="true"
@@ -124,101 +160,67 @@ const NavBar = () => {
                 </Avatar>
               </IconButton>
             </Tooltip>
-             
           )}
-         
         </Toolbar>
       </AppBar>
-
-      <Drawer
-        anchor="left"
-        open={drawerOpen}
-        onClose={handleDrawerClose}
-        PaperProps={{
-          sx: {
-            width: "250px",
-            height: "100%",
-          },
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            marginTop: "15px",
-            alignItems: "center",
-            paddingX: 2,
+      <Box component="nav">
+        <Drawer
+          variant="temporary"
+          open={isNavBarOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          PaperProps={{
+            style: {
+              boxSizing: "border-box",
+              width: isSmallScreen ? "60vw" : "20vw",
+            },
           }}
         >
-          <Typography variant="h6">VANA Jewellery</Typography>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon sx={{ color: "#bd8d67" }} />
-          </IconButton>
-        </Box>
-
-        <List>
-          {menuItems.map((item, index) => (
-            <ListItem
-              key={index}
-              onClick={() => handlePageNavigate(item.path)}
-              sx={{
-                cursor: "pointer",
-                "&:hover": {
-                  backgroundColor: "#e0e0e0",
-                },
-              }}
-            >
-              {item.icon && (
-                <ListItemIcon
-                  sx={{ minWidth: 40, marginRight: 0, color: "#bd8d67" }}
-                >
-                  {item.icon}
-                </ListItemIcon>
-              )}
-              <ListItemText primary={item.text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+          {/* <NavbarDrawer onDrawerToggle={handleDrawerToggle} /> */}
+        </Drawer>
+      </Box>
       <Menu
-             anchorEl={anchorEl}
-             id="account-menu"
-             open={open}
-             onClose={handleMenuClose}
-             PaperProps={{
-               elevation: 0,
-               sx: {
-                 overflow: "visible",
-                 filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                 "& .MuiAvatar-root": {
-                   width: 25,
-                   height: 25,
-                   mr: 1,
-                 },
-                 "&:before": {
-                   content: '""',
-                   display: "block",
-                   position: "absolute",
-                   top: 0,
-                   right: 14,
-                   width: 10,
-                   height: 10,
-                   bgcolor: "background.paper",
-                   transform: "translateY(-50%) rotate(45deg)",
-                   zIndex: 0,
-                 },
-               },
-             }}
-             transformOrigin={{ horizontal: "right", vertical: "top" }}
-             anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-           >
-             <MenuItem onClick={handleLogoutClick}>
-               <ListItemIcon>
-                 <Logout fontSize="small" />
-               </ListItemIcon>
-               Logout
-             </MenuItem>
-           </Menu>
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            "& .MuiAvatar-root": {
+              width: 25,
+              height: 25,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              // bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem onClick={handleLogoutClick}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
     </>
   );
 };
